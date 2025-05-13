7c9d0e0b102964cb22551acab84cf9756801bb2b
Shay-Webdev <181019353+Shay-Webdev@users.noreply.github.com>
2025-05-14 00:08:51 +0530

    Feature: home page , user posts

--- src/Pages/Posts/Posts.tsx
+++ src/Pages/Posts/Posts.tsx
@@ -2,18 +2,14 @@ import styles from "./Posts.module.css";
 import { useEffect, useState } from "react";
 import { userInSession } from "../../utilities/userInSession";
 import { type fetchWrapperParam, getApi } from "../../utilities/fetchWrapper";
-import { urlPaths } from "../../utilities/urlPaths";
 import { LoadingPage } from "../LoadingPage/LoadingPage";
 import { ErrorBoundaryWrapper } from "../Error/Error";
 import { useErrorBoundary } from "react-error-boundary";
 import { Cards } from "../../components/Cards/Cards";
-import { boolean, coerce, number, object, string, z } from "zod";
+import { boolean, coerce, number, string, z } from "zod";
 
 const postSchema = z.object({
   authorId: number(),
-  author: object({
-    username: string(),
-  }),
   id: number(),
   isPublished: boolean(),
   publishedDate: coerce.date(),
@@ -27,13 +23,14 @@ type Post = z.infer<typeof postSchema>;
 const Posts = () => {
   const [status, setStatus] = useState<"done" | "pending">("pending");
   const [posts, setPosts] = useState<Post[]>([]);
+  const [username, setUserName] = useState<string | undefined>(undefined);
   const { showBoundary } = useErrorBoundary();
   useEffect(() => {
     async function asyncHandler() {
       try {
         const user = await userInSession();
         const postsParams: fetchWrapperParam = {
-          url: urlPaths.postUrl.posts,
+          url: `http://localhost:3000/posts/${user?.user.sub}/posts`,
           opts: {
             headers: {
               authorization: `Bearer ${user?.accessToken}`,
@@ -50,12 +47,9 @@ const Posts = () => {
           throw new Error(`invalid post data `);
         }
 
+        setUserName(user?.user.username);
         setPosts(parsedPosts.data);
 
-        console.log(
-          `trimmed post content: `,
-          posts.map((post) => post.content.split(" ").splice(0, 10).join(" ")),
-        );
         setStatus("done");
       } catch (error) {
         showBoundary(error);
@@ -68,12 +62,15 @@ const Posts = () => {
   }
   return (
     <section className={styles.posts_container}>
-      <h1>See what others have to share</h1>
+      <h1 className={styles.posts_heading}>
+        Welcome <span>{username}</span>, <br />
+        Here are your posts
+      </h1>
       <section className={styles.cards_container}>
         {posts.map((post) => {
           return (
             <Cards
-              author={post.author.username}
+              author={username as string}
               postPreview={post.content.split(" ").splice(0, 10).join(" ")}
               postTitle={post.title}
               postId={post.id}
